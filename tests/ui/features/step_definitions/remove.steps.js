16a8b30c72c0527a284689bbb81961a207d09922
Guillaume Prin <guil.prin@gmail.com>
2015-12-14 14:12:12 +0100

    added Given and When

--- tests/ui/features/step_definitions/remove.steps.js
+++ tests/ui/features/step_definitions/remove.steps.js
@@ -4,18 +4,28 @@ var Browser = require('zombie');
 module.exports = function() {
     'use strict';
 
-    var _browser = Browser.create();
+    var _browser = new Browser();
 
     this.Given(/^The list of contacts is correctly displayed$/, function(callback) {
-        
-    }):
+        _browser.visit("http://127.0.0.1:3000", function(err) {
+            _browser.assert.text("h1", "Contacts");
+            callback();
+        });
+    });
 
     this.When(/^I click on the first Delete Button$/, function(callback) {
         _browser.visit("http://127.0.0.1:3000", function(err) {
             _browser.pressButton('Delete', function(err) {
                 if (err) throw err;
-                callback.pending();
+                callback();
             });
+        });
+    });
+
+
+    this.Then(/^The first contact is deleted$/, function(callback) {
+        _browser.visit("http://127.0.0.1:3000", function(err) {
+            callback.pending();
         });
     });
 
